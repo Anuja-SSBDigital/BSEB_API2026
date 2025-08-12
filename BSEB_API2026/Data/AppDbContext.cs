64b256922670c6b44c8bcd007826e5aaa8d76689
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-12 15:03:01 +0530

    add changes

---
+++ BSEB_API2026/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BSEB_API2026.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSEB_API2026.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<StudentMaster> StudentMaster { get; set; }
+    }
+}
--- BSEB_API2026/Program.cs
+++ BSEB_API2026/Program.cs
@@ -1,15 +1,26 @@
+using BSEB_API2026.Data;
+using BSEB_API2026.Middleware;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DI registrations
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline (error handler first)
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
