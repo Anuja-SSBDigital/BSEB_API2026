6c14341455a0fde2c1ea4ae8d5c73da285558398
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-12 15:03:58 +0530

    Add middleware

---
+++ BSEB_API2026/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace BSEB_API2026.Middleware
+{
+    public class ErrorHandlerMiddleware(RequestDelegate _next)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                HttpResponse response = context.Response;
+                response.ContentType = "application/json";
+                response.StatusCode = error switch
+                {
+                    ApplicationException => (int)HttpStatusCode.BadRequest,
+                    KeyNotFoundException => (int)HttpStatusCode.NotFound,
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+
+                // _logger.WriteError($"Exception: {JsonSerializer.Serialize(new { message = error?.Message })} \n StackTrace: {error?.StackTrace}");
+
+                await response.WriteAsync(JsonSerializer.Serialize(new { message = error?.Message }));
+            }
+        }
+    }
+}
