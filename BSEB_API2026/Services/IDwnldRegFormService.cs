338c22fea29f01619a4a691916c6dbf614df313a
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-18 17:18:42 +0530

    change in services

--- BSEB_API2026/Services/IDwnldRegFormService.cs
+++ BSEB_API2026/Services/IDwnldRegFormService.cs
@@ -4,7 +4,7 @@ namespace BSEB_API2026.Services
 {
     public interface IDwnldRegFormService
     {
-        Task<List<StudentDTO>> GetStudentDataAsync(int collegeId, string collegeCode, string studentName, int facultyId);
+        Task<List<StudentDTO>> GetStudentDataAsync(string? collegeId, string collegeCode, string studentName, int facultyId);
         Task<List<StudentExtendedDTO>> GetStudentDataforPayment(int collegeId, string collegeCode, string studentName, int facultyId,string subcategory);
     }
 }
