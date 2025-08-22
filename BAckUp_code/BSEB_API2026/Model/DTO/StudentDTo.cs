fcb5c30cacfe9a563521669ec922e0b00857aee1
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-22 16:57:37 +0530

    add model class

---
+++ BAckUp_code/BSEB_API2026/Model/DTO/StudentDTo.cs
@@ -0,0 +1,17 @@
+﻿namespace BSEB_API2026.Model.DTO
+{
+    public class StudentDTo
+    {
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public string? FatherName { get; set; }
+        public string? MotherName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? Faculty { get; set; }
+        public string? College { get; set; }
+        public bool? FormDownloaded { get; set; }
+
+        public int? FacultyId { get; set; }
+        public int? CollegeId { get; set; }
+    }
+}
