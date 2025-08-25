4d77ed059d6c9ad09a81978a8aaa6719cfeb731d
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-25 17:08:46 +0530

    display subject data

---
+++ BSEB_API2026/Model/SubjectPaperResult.cs
@@ -0,0 +1,12 @@
+﻿namespace BSEB_API2026.Model
+{
+    public class SubjectPaperResult
+    {
+        public string GroupName { get; set; }
+        public string GroupNameHindi { get; set; }
+        public int SubjectPaperCode { get; set; }
+        public string SubjectPaperName { get; set; }
+        public int Fk_SubjectGroupId { get; set; }
+        public int Pk_SubjectPaperId { get; set; }
+    }
+}
