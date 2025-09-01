3107ac7409285e1d071f59efc27fc95826d7350a
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-09-01 18:04:48 +0530

    Add model

---
+++ BSEB_API2026/Model/DTO/GetStudentRegiListData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BSEB_API2026.Model.DTO
+{
+#pragma warning disable 
+
+    [Keyless] 
+    public class GetStudentRegiListData
+    {
+        public string CollegeCode { get; set; }          
+        public string StudentName { get; set; }         
+     
+        public string FatherName { get; set; }          
+        public string MotherName { get; set; }          
+       public string YearOfPassing { get; set; }          
+     
+    }
+}
