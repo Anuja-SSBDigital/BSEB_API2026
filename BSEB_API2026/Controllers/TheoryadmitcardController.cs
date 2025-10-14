c78a7f928678d9f2c0f446968a93222f91c040ca
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-10-14 18:28:36 +0530

    changes

--- BSEB_API2026/Controllers/TheoryadmitcardController.cs
+++ BSEB_API2026/Controllers/TheoryadmitcardController.cs
@@ -26,7 +26,9 @@ namespace BSEB_API2026.Controllers
             var data = await _service.GetFacultiesAsync();
             return Ok(data);
         }                    
-                                           
+           
+        
+
         [HttpGet("TheoryAdmitCardsStudentLists")]
         public async Task<IActionResult> GetStudents([FromQuery] string collegeId, [FromQuery] string facultyId)
         {
@@ -37,12 +39,12 @@ namespace BSEB_API2026.Controllers
             return Ok(data);
         }
 
-
-      
         [NonAction]
         [HttpPost("download-admitcards")]
         public IActionResult DownloadAdmitCards([FromBody] List<StudentDto> selectedStudents)
         {
+
+
 
             if (selectedStudents == null || selectedStudents.Count == 0)
                 return BadRequest(new { message = "Please select at least one student" });
