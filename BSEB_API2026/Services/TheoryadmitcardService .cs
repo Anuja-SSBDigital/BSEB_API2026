4d66a8f1a72cddbaf2fb6b570a837226bfd45650
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-11-07 18:09:40 +0530

    changes

--- BSEB_API2026/Services/TheoryadmitcardService .cs
+++ BSEB_API2026/Services/TheoryadmitcardService .cs
@@ -28,12 +28,7 @@ namespace BSEB_API2026.Services
             using var conn = new SqlConnection(GetConnString());
             await conn.OpenAsync();
 
-            //const string sql = @"SELECT Pk_FacultyId, FacultyName FROM dbo.Faculty_Mst";
-
-            //using var cmd = new SqlCommand(sql, conn) { CommandType = CommandType.Text };
-            //using var reader = await cmd.ExecuteReaderAsync();
-
-
+         
             const string sql = @"SELECT Pk_FacultyId, FacultyName FROM dbo.Faculty_Mst";
 
             using var cmd = new SqlCommand(sql, conn) { CommandType = CommandType.Text };
@@ -86,8 +81,6 @@ WHERE (@CollegeId IS NULL OR stu.Fk_CollegeId = @CollegeId)
 ORDER BY stu.Pk_StudentId DESC;
 ";
 
-
-
             using var cmd = new SqlCommand(sql, conn) { CommandType = CommandType.Text };
             cmd.Parameters.Add("@CollegeId", SqlDbType.VarChar, 50).Value = (object?)collegeId ?? DBNull.Value;
             cmd.Parameters.Add("@FacultyId", SqlDbType.VarChar, 50).Value = (object?)facultyId ?? DBNull.Value;
@@ -103,10 +96,18 @@ ORDER BY stu.Pk_StudentId DESC;
                     StudentFullName = reader["StudentFullName"]?.ToString(),
                     FatherName = reader["FatherName"]?.ToString(),
 
-
-                  
                     MotherName = reader["MotherName"]?.ToString(),
-                  
+
+                    //DOB = reader["DOB"]?.ToString(),
+
+                    //CollegeId = reader["CollegeId"]?.ToString(),
+                    //CollegeName = reader["CollegeName"]?.ToString(),
+                    //FacultyId = reader["FacultyId"]?.ToString(),
+                    //FacultyName = reader["FacultyName"]?.ToString(),
+                    //ExamTypeId = reader["ExamTypeId"]?.ToString(),
+                    //IsRegCardUploaded = reader["IsRegCardUploaded"] is not DBNull &&
+                    //                 Convert.ToBoolean(reader["IsRegCardUploaded"])
+
                     DOB = reader["DOB"]?.ToString(),
 
                     CollegeId = reader["CollegeId"]?.ToString(),
