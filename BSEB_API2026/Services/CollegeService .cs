5e4fcf74dc47abba09495aa720e8605cf08eafa0
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-11-15 17:57:08 +0530

    changes

--- BSEB_API2026/Services/CollegeService .cs
+++ BSEB_API2026/Services/CollegeService .cs
@@ -10,6 +10,7 @@ namespace CollegeSeatAPI.Services
     {
         private readonly AppDbContext _db;
 
+
         public CollegeService(AppDbContext db)
         {
             _db = db;
@@ -29,28 +30,12 @@ namespace CollegeSeatAPI.Services
              
             var mode = regMode.Trim().ToLowerInvariant();
 
-
             switch (mode)
             {
                 case "ofss":
                     if (string.IsNullOrWhiteSpace(categoryType))
                         categoryType = "Regular";
                     break;
-
-
-                //case "non-ofss":
-                //    if (string.IsNullOrWhiteSpace(categoryType))
-                //        categoryType = "Private";
-                //    break;
-
-                //case "display-registered":
-                //    if (string.IsNullOrWhiteSpace(categoryType))
-                //        throw new ArgumentException(
-                //            "For regMode=display-registered, categoryType is required (e.g., 'Regular' or 'Private').",
-                //            nameof(categoryType));
-                //    break;
-
-
 
 
                 case "non-ofss":
@@ -65,14 +50,11 @@ namespace CollegeSeatAPI.Services
                             nameof(categoryType));
                     break;
 
-
                 default:
                     throw new ArgumentException(
                         "regMode must be one of: ofss, non-ofss, display-registered.",
                         nameof(regMode));
             }
-
-
 
             var rows = await _db.Set<GetStudentRegiListData>()
               .FromSqlInterpolated($@"
@@ -86,7 +68,6 @@ namespace CollegeSeatAPI.Services
             return rows;
 
         }
-
 
         public async Task<IEnumerable<FacultyDto>> GetFacultyDropdownAsync()
         {
