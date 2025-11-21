11f7f595388391ef24c1d45fe7c3f1152653e874
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-11-21 17:59:15 +0530

    Add Model

---
+++ BSEB_API2026/Model/DTO/FacultyDto.cs
@@ -0,0 +1,28 @@
+﻿namespace BSEB_API2026.Model.DTO
+
+#pragma warning disable
+{
+    public class FacultyDto
+    {
+        public string FacultyId { get; set; }
+        public string FacultyName { get; set; }
+    }
+    public class StudentDto
+    {
+       
+        public string StudentId { get; set; }
+        public string CollegeId { get; set; }
+        public string CollegeName { get; set; }
+        public string FacultyId { get; set; }
+        public string FacultyName { get; set; }
+        public string ExamTypeId { get; set; }
+        public bool IsRegCardUploaded { get; set; }
+
+        public string StudentFullName { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string DOB { get; set; }   // keep as
+
+
+    }
+}
