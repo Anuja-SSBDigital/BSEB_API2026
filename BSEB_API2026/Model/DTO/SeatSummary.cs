b8390bef3e60ba9d9fdf3fd9331dcc100690061f
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-11-21 18:02:45 +0530

    add model

---
+++ BSEB_API2026/Model/DTO/SeatSummary.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_API2026.Model.DTO
+{
+    public class SeatSummary
+    {
+        
+        public int Id { get; set; }                  
+        public int RemainingRegularSeats { get; set; }
+        public int RemainingPrivateSeats { get; set; }
+        public int TotalPaymentDone { get; set; }
+        public int TotalFormSubmitted { get; set; }
+        public int PaymentDoneFormNotSubmitted { get; set; }
+    }
+
+    public class Student
+    {
+       
+        public int Id { get; set; } 
+        public string StudentName { get; set; } = string.Empty;
+        public string CategoryType { get; set; } = "Regular";
+        public string RegistrationMode { get; set; } = string.Empty;
+
+        public int CollegeId { get; set; }
+        public int FacultyId { get; set; }
+    }
+
+    public class StudentDetails
+    {
+     
+        public int Id { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string CategoryType { get; set; } = string.Empty;
+        public string FatherName { get; set; } = string.Empty;
+        public string Mobile { get; set; } = string.Empty;
+      
+    }
+}
---
+++ BSEB_API2026/Model/DTO/SeatSummaryDto.cs
@@ -0,0 +1,25 @@
+﻿namespace BSEB_API2026.Model.DTO
+{
+    public class SeatSummaryDto
+    {
+        public int RemainingRegularSeats { get; set; }
+        public int RemainingPrivateSeats { get; set; }
+        public int TotalPaymentDone { get; set; }
+        public int TotalFormSubmitted { get; set; }
+        public int PaymentDoneFormNotSubmitted { get; set; }
+    }
+
+    public class FacultyDto2
+    {
+        public int FacultyId { get; set; }
+        public string FacultyName { get; set; } = string.Empty;
+    }
+
+    public class StudentDto2
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string CategoryType { get; set; } = "Regular";
+        public string RegistrationMode { get; set; } = string.Empty;
+    }
+}
