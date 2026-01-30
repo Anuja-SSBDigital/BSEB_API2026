37b7836e382c3766b486baf9d7514eda8ffef638
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-30 18:01:33 +0530

    changes

--- BSEB_API2026/Services/CollegeService .cs
+++ BSEB_API2026/Services/CollegeService .cs
@@ -15,7 +15,6 @@ namespace CollegeSeatAPI.Services
             _db = db;
         } 
 
-
         public async Task<IEnumerable<GetStudentRegiListData>> GetStudentsAsync(
             int collegeId,
             int facultyId,     
@@ -32,9 +31,7 @@ namespace CollegeSeatAPI.Services
             var mode = regMode.Trim().ToLowerInvariant();
 
             switch (mode)
-            {
-
-
+            {       
                 case "ofss":
                     if (string.IsNullOrWhiteSpace(categoryType))
                         categoryType = "Regular";
@@ -52,26 +49,6 @@ namespace CollegeSeatAPI.Services
                             "For regMode=display-registered, categoryType is required (e.g., 'Regular' or 'Private').",
                             nameof(categoryType));
                     break;
-
-
-
-                //case "ofss":
-                //    if (string.IsNullOrWhiteSpace(categoryType))
-                //        categoryType = "Regular";
-                //    break;
-
-
-                //case "non-ofss":
-                //    if (string.IsNullOrWhiteSpace(categoryType))
-                //        categoryType = "Private";
-                //    break;
-
-                //case "display-registered":
-                //    if (string.IsNullOrWhiteSpace(categoryType))
-                //        throw new ArgumentException(
-                //            "For regMode=display-registered, categoryType is required (e.g., 'Regular' or 'Private').",
-                //            nameof(categoryType));
-                //    break;
 
                 default:
                     throw new ArgumentException(
