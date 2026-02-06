d4ecc1faa016e12cb8ec13151aba3127021fa58b
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-02-06 17:57:15 +0530

    changes

--- BSEB_API2026/Services/CollegeService .cs
+++ BSEB_API2026/Services/CollegeService .cs
@@ -23,16 +23,17 @@ namespace CollegeSeatAPI.Services
             string? studentName)   
         {
 
-
             if (string.IsNullOrWhiteSpace(regMode))
                 throw new ArgumentException(
                     "regMode is required and must be one of: ofss, non-ofss, display-registered.",
                     nameof(regMode));      
 
+
             var mode = regMode.Trim().ToLowerInvariant();
 
             switch (mode)
             {
+
 
                 case "ofss":
                     if (string.IsNullOrWhiteSpace(categoryType))
@@ -45,6 +46,8 @@ namespace CollegeSeatAPI.Services
                         categoryType = "Private";
                     break;
 
+
+
                 case "display-registered":
                     if (string.IsNullOrWhiteSpace(categoryType))
                         throw new ArgumentException(
@@ -53,11 +56,32 @@ namespace CollegeSeatAPI.Services
                     break;
 
 
+
+                //case "ofss":
+                //    if (string.IsNullOrWhiteSpace(categoryType))
+                //        categoryType = "Regular";
+                //    break;
+
+
+                //case "non-ofss":
+                //    if (string.IsNullOrWhiteSpace(categoryType))
+                //        categoryType = "Private";
+                //    break;
+
+                //case "display-registered":
+                //    if (string.IsNullOrWhiteSpace(categoryType))
+                //        throw new ArgumentException(
+                //            "For regMode=display-registered, categoryType is required (e.g., 'Regular' or 'Private').",
+                //            nameof(categoryType));
+                //    break;
+
+
                 default:
                     throw new ArgumentException(
                         "regMode must be one of: ofss, non-ofss, display-registered.",
                         nameof(regMode));
             }
+
 
 
             var rows = await _db.Set<GetStudentRegiListData>()
@@ -72,6 +96,8 @@ namespace CollegeSeatAPI.Services
             return rows;
 
         }
+
+
 
         public async Task<IEnumerable<FacultyDto>> GetFacultyDropdownAsync()
         {
