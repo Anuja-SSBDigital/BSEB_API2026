cd51fc6e69dfe9adf899905186933be0539e3f73
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-05-08 18:00:19 +0530

    changes

--- BSEB_API2026/Services/CollegeService .cs
+++ BSEB_API2026/Services/CollegeService .cs
@@ -15,6 +15,7 @@ namespace CollegeSeatAPI.Services
             _db = db;
         } 
 
+
         public async Task<IEnumerable<GetStudentRegiListData>> GetStudentsAsync(
             int collegeId,
             int facultyId,     
@@ -22,6 +23,7 @@ namespace CollegeSeatAPI.Services
             string? categoryType,           
             string? studentName)   
         {
+
 
             if (string.IsNullOrWhiteSpace(regMode))
                 throw new ArgumentException(
@@ -32,6 +34,19 @@ namespace CollegeSeatAPI.Services
 
             switch (mode)
             {
+                //case "ofss":
+                //    if (string.IsNullOrWhiteSpace(categoryType))
+                //        categoryType = "Regular";
+                //    break;
+
+
+                //case "non-ofss":
+                //    if (string.IsNullOrWhiteSpace(categoryType))
+                //        categoryType = "Private";
+                //    break;
+
+
+
                 case "ofss":
                     if (string.IsNullOrWhiteSpace(categoryType))
                         categoryType = "Regular";
@@ -50,6 +65,7 @@ namespace CollegeSeatAPI.Services
                             "For regMode=display-registered, categoryType is required (e.g., 'Regular' or 'Private').",
                             nameof(categoryType));
                     break;
+
 
 
                 default:
